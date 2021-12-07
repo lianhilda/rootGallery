8744865c80aa6a75c53411b6e2365b4e98913aa6
lianhilda <771601423@qq.com>
2021-12-07 23:36:08 +0800

    3-12事件处理

---
+++ src/components/Robot.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './Robot.module.css'
+
+interface RobotProps {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  return (
+    <li className={style.cardContainer}>
+      <img src={`https://robohash.org/${id}`}/>
+      <h2>name</h2>
+      <p>email</p>
+    </li>
+  );
+};
+
+export default Robot;
