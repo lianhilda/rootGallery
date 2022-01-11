44e1280101449b67d3e9500b2d8d606d79c1fc0c
lianhilda <771601423@qq.com>
2022-01-11 20:55:43 +0800

    useEffect模拟生命周期示范

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../src/assets/images/logo.svg";
 import styles from "./App.module.css";
 import robots from "./mockdata/robots.json";
@@ -10,37 +10,41 @@ interface Props {}
 interface State {
   robotGallery: any;
 }
+const App: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number>(0);
+  const [robotGallery, setRobotGallery] = useState<any>([]);
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
+    document.title = `点击了${count}次`;
+  }, [count]);
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
+      .then((data) => setRobotGallery(data));
+  }, []);
 
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-        </div>
-        <ShoppingCart />
-        <div className={styles.robotList}>
-          {this.state.robotGallery.map((r) => (
-            <Robot id={r.id} name={r.name} email={r.email} />
-          ))}
-        </div>
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
       </div>
-    );
-  }
-}
+      <ShoppingCart />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
+      <span>count: {count}</span>
+      <div className={styles.robotList}>
+        {robotGallery.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
